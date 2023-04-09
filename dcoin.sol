dee2bee384538bd4b450691a91f0ee1123bb3ad3
abisheg0213 <109604992+abisheg0213@users.noreply.github.com>
2023-04-09 21:51:18 +0530

    solidity works

---
+++ dcoin.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.8.14;
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint);
+    function approve(address spender, uint amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+contract dummy is IERC20
+{
+    uint  totalsuplly;
+    mapping(address => uint) balanceof;
+    mapping(address=> mapping (address=>uint))public allowance;
+    string  name="dcoin";
+    string  symbol="d@!c";
+    uint decimals=18;
+    constructor(uint total)
+    {
+        totalsuplly=total;
+        balanceof[msg.sender]=totalsuplly;
+    }
+    function totalSupply() external view returns (uint)
+    {
+        return totalsuplly;
+    }
+    function balanceOf(address account) external view returns (uint)
+    {
+        return balanceof[account];
+    }
+    function transfer(address recipient, uint amount) external returns (bool)
+    {
+        balanceof[msg.sender]-=amount;
+        balanceof[recipient]+=amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+    function approve(address spender, uint amount) external returns (bool)
+    {
+        allowance[msg.sender][spender]=amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool)
+    {
+        balanceof[sender]-=amount;
+        allowance[sender][msg.sender]-=amount;
+        balanceof[recipient]+=amount;
+        return true;
+    }
+}
